85eafdf6ac5be11f375eaaaa177b60a23928eb88
Lahaxe Hugo <lahaxeh@gmail.com>
2023-04-04 21:22:34 +0200

    adding experiences block

---
+++ src/components/Diplomas.vue
@@ -0,0 +1,35 @@
+<template>
+  <Title>Formation</Title>
+  <div class="skills-wrapper">
+    <div v-for="(diploma, index) in diplomas" :key="diploma.name">
+      <h3 class="title2">{{ diploma.name }}</h3>
+      <p class="title3">{{ diploma.school }}</p>
+      <p class="text2">
+        {{ diploma['date-from'] }} - {{ diploma['date-to'] }}, {{ diploma.location }}
+      </p>
+      <p>{{ diploma.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Title from './Title.vue'
+import DiplomasData from '../data/diplomas.json'
+
+export default {
+  components: {
+    Title
+  },
+  data() {
+    return {
+      diplomas: DiplomasData
+    }
+  }
+}
+</script>
+
+<style scoped>
+.skills-wrapper > div:nth-child(n + 2) {
+  margin-top: 1.5rem;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="home">
     <Skills />
+    <Diplomas />
   </div>
 </template>
 
 <script setup>
 import Skills from '../components/Skills.vue'
+import Diplomas from '../components/Diplomas.vue'
 </script>
