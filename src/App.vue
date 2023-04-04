a6589f461ee286378a24ff53fec7cc5314db13cb
Lahaxe Hugo <lahaxeh@gmail.com>
2023-04-04 17:29:00 +0200

    vue init

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/contact">Contact</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  margin-bottom: 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 16px;
+  text-align: center;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+  </div>
+</template>
