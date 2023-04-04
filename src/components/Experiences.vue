e1494172f42fb6c9ee28bb82dce0c21c02df4dbf
Lahaxe Hugo <lahaxeh@gmail.com>
2023-04-04 21:39:41 +0200

    adding experiences block

---
+++ src/components/Experiences.vue
@@ -0,0 +1,46 @@
+<template>
+  <Title>Expériences</Title>
+  <div class="experiences-wrapper">
+    <div v-for="(experience, index) in experiences" :key="experience.name">
+      <h3 class="title2">{{ experience.name }}</h3>
+      <p class="title3">{{ experience.company }}</p>
+      <p class="text2">
+        {{ experience['date-from']
+        }}{{
+          experience['date-from'] !== experience['date-to'] ? ' - ' + experience['date-to'] : ''
+        }}, {{ experience.location }}
+      </p>
+      <ul class="details">
+        <li v-for="(detail, index) in experience.details" :key="index">
+          {{ detail }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import Title from './Title.vue'
+import ExperiencesData from '../data/experiences.json'
+
+export default {
+  components: {
+    Title
+  },
+  data() {
+    return {
+      experiences: ExperiencesData
+    }
+  }
+}
+</script>
+
+<style scoped>
+.experiences-wrapper > div:nth-child(n + 2) {
+  margin-top: 1.5rem;
+}
+
+.details {
+  padding-left: 20px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,12 @@
   <div class="home">
     <Skills />
     <Diplomas />
+    <Experiences />
   </div>
 </template>
 
 <script setup>
 import Skills from '../components/Skills.vue'
 import Diplomas from '../components/Diplomas.vue'
+import Experiences from '../components/Experiences.vue'
 </script>
